1a0da09295d552adc75e1545a9c41e04e641adad
giosue2007 <toxicgiosue@gmail>
2026-04-07 22:48:59 +0200

    Add task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,21 +9,21 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *new_node;
 
-	if (!parent)
+	if (parent == NULL)
 		return (NULL);
 
-	new = binary_tree_node(parent, value);
-	if (!new)
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
 		return (NULL);
 
-	if (parent->left)
+	if (parent->left != NULL)
 	{
-		new->left = parent->left;
-		new->left->parent = new;
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
 	}
-	parent->left = new;
+	parent->left = new_node;
 
-	return (new);
+	return (new_node);
 }
